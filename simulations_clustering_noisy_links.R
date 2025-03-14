a62bab7eb6b4546eca0d9f44dfdab50ed1627578
Louis <louis.lacoste@agroparistech.fr>
2025-03-14 09:26:34 +0100

    Fix plan

--- simulations_clustering_noisy_links.R
+++ simulations_clustering_noisy_links.R
@@ -47,7 +47,7 @@ conditions <- expand.grid(
     rep = seq_len(n_reps)
 )
 
-plan(list(tweak("callr", workers = floor(parallelly::availableCores(omit = 1L) - rep_clustering * 3L)), tweak("callr", workers = 5L), tweak("callr", workers = 3L)))
+plan(list(tweak("callr", workers = floor(parallelly::availableCores(omit = 1L) / rep_clustering * 3L)), tweak("callr", workers = 5L), tweak("callr", workers = 3L)))
 
 set.seed(1234)
 df_list <- future_lapply(seq_len(nrow(conditions)), function(s) {
