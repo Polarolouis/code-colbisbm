191018d9f77ec96a302f67953e4ab7f78d4ad814
Louis <louis.lacoste@agroparistech.fr>
2025-03-24 11:47:12 +0100

    Adding struct to conditions in NA robustness sims Separating colSBM models in plots in analyze

---
+++ analyze_NA_robustness.R
@@ -0,0 +1,149 @@
+## ----libraries, echo = FALSE, include = FALSE----------------------------------------------------------------------------------------------------------------------------------------
+library("ggplot2")
+library("ggokabeito")
+library("tidyr")
+library("dplyr")
+library("stringr")
+library("knitr")
+library("kableExtra")
+library("vctrs")
+library("stringr")
+library("here")
+library("tikzDevice")
+options(tikzDocumentDeclaration = "\\documentclass[10pt]{standalone}")
+
+filenames <- here("simulations", "NA_robustness", "NA_robustness_25-07-2024_10-43-12_uniform_modular_1-400.Rds")
+
+results_df <- readRDS(filenames)
+
+matching_filenames <- filenames |>
+    grep(pattern = "(modular|nested)", value = TRUE) |>
+    head(n = 2)
+
+results_df <- results_df |>
+    select(-c(repetition, sampling))
+
+auc_df <-
+    results_df |>
+    select(-contains(c("ari", "elapsed_secs"))) |>
+    pivot_longer(
+        cols = c(auc_LBM, auc_colBiSBM),
+        values_to = "auc",
+        names_prefix = "auc_",
+        names_to = "method"
+    ) |>
+    mutate(model = ifelse(method == "LBM", paste0("sep-", model), model)) |>
+    mutate_at(vars(model, struct), as.factor) |>
+    mutate(model = forcats::fct_relevel(
+        model,
+        "iid", "sep-iid", "pi", "sep-pi", "rho", "sep-rho", "pirho", "sep-pirho"
+    )) |>
+    filter(prop_NAs <= 0.7)
+
+levels(auc_df[["model"]]) <- levels(auc_df[["model"]]) |>
+    str_replace(fixed("iid"), "$iid$") |>
+    str_replace(fixed("pirho"), "$\\pi\\rho$") |>
+    str_replace("pi$", "$\\\\pi$") |>
+    str_replace("rho$", "$\\\\rho$")
+
+lapply(c("iid", "\\pi", "\\rho", "\\pi\\rho"), function(model_name) {
+    color <- switch(model_name,
+        iid = 2,
+        "\\pi" = 3,
+        "\\rho" = 4,
+        "\\pi\\rho" = 5
+    )
+
+    auc_df |>
+        filter(str_equal(model, paste0("$", model_name, "$")) | str_equal(model, paste0("sep-$", model_name, "$"))) |>
+        # Remove model_name from sep-model_name
+        mutate(model = str_replace(model, fixed(paste0("sep-$", model_name, "$")), "sep")) |>
+        ggplot() +
+        aes(x = factor(prop_NAs), y = auc, fill = model, color = model) +
+        geom_boxplot(notch = TRUE) +
+        labs(x = "$p_{\\texttt{NA}}$", y = "AUC") +
+        scale_fill_okabe_ito(order = c(color, 1), alpha = 0.5) +
+        scale_color_okabe_ito(order = c(color, 1)) +
+        guides(
+            fill = guide_legend(title = "Model"),
+            color = guide_legend(title = "Model")
+        ) +
+        theme_minimal()
+}) -> auc_plots
+
+output_tikz_folder <- here(
+    "mia-rapport-2024", "tikz", "simulations",
+    "na_robustness"
+)
+if (!dir.exists(output_tikz_folder)) {
+    dir.create(output_tikz_folder, recursive = TRUE)
+}
+
+tikz(
+    file = file.path(output_tikz_folder, "auc-model.tex"), width = 6.5,
+    height = 3.5,
+    standAlone = TRUE
+)
+print(auc_plot)
+dev.off()
+## ARI plots
+ari_df <- results_df |>
+    select(-contains(c("auc", "elapsed_secs"))) |>
+    group_by(prop_NAs, model) |>
+    pivot_longer(
+        cols = c(arirow_LBM:aricol_colBiSBM),
+        values_to = "ari", names_to = c("dim", "method"),
+        names_pattern = "(row|col)_(LBM|colBiSBM)"
+    ) |>
+    mutate(model = ifelse(method == "LBM", paste0("sep-", model), model)) |>
+    mutate_at(vars(model, struct, dim, method), as.factor) |>
+    mutate(model = forcats::fct_relevel(
+        model,
+        "iid", "sep-iid", "pi", "sep-pi", "rho", "sep-rho", "pirho", "sep-pirho"
+    )) |>
+    filter(prop_NAs <= 0.7)
+
+levels(ari_df[["model"]]) <- levels(ari_df[["model"]]) |>
+    str_replace(fixed("iid"), "$iid$") |>
+    str_replace(fixed("pirho"), "$\\pi\\rho$") |>
+    str_replace("pi$", "$\\\\pi$") |>
+    str_replace("rho$", "$\\\\rho$")
+
+ari_df[["dim"]] <- relevel(ari_df[["dim"]], ref = "row")
+dim.labs <- c("Rows : $d = 1$", "Columns : $d = 2$")
+names(dim.labs) <- c("row", "col")
+
+lapply(c("iid", "\\pi", "\\rho", "\\pi\\rho"), function(model_name) {
+    color <- switch(model_name,
+        iid = 2,
+        "\\pi" = 3,
+        "\\rho" = 4,
+        "\\pi\\rho" = 5
+    )
+
+    ari_df |>
+        filter(str_equal(model, paste0("$", model_name, "$")) | str_equal(model, paste0("sep-$", model_name, "$"))) |>
+        # Remove model_name from sep-model_name
+        mutate(model = str_replace(model, fixed(paste0("sep-$", model_name, "$")), "sep")) |>
+        ggplot() +
+        aes(x = factor(prop_NAs), y = ari, fill = model, color = model) +
+        geom_boxplot(notch = TRUE) +
+        labs(x = "$p_{\\texttt{NA}}$", y = "ARI") +
+        guides(
+            fill = guide_legend(title = "Model"),
+            color = guide_legend(title = "Model")
+        ) +
+        scale_fill_okabe_ito(order = c(color, 1), alpha = 0.5) +
+        scale_color_okabe_ito(order = c(color, 1)) +
+        facet_grid(struct ~ dim, labeller = labeller(dim = dim.labs)) +
+        theme_minimal() +
+        theme(axis.text.x = element_text(size = 5))
+}) -> ari_plots
+ari_plot <- wrap_plots(ari_plots) + plot_annotation(tag_levels = "A")
+tikz(
+    file = file.path(output_tikz_folder, "ari-dim-model.tex"), width = 6.5,
+    height = 4,
+    standAlone = TRUE
+)
+print(ari_plot)
+dev.off()
--- simulations_NA_robustness.R
+++ simulations_NA_robustness.R
@@ -7,15 +7,6 @@ options(future.globals.maxSize = Inf)
 library("pROC")
 library("colSBM")
 
-sampling <- "uniform"
-struct <- "modular"
-#  Arguments checks
-allowed_struct <- c("modular", "nested")
-
-stopifnot(
-    "Unknown structure, should be : modular or nested" = (struct %in% allowed_struct)
-)
-
 set.seed(1234)
 
 eps <- 0.05
@@ -30,85 +21,141 @@ nc2 <- 120
 pir <- c(0.5, 0.3, 0.2)
 pic <- c(0.5, 0.3, 0.2)
 
-struct <- "modular"
+alpha_modular <- matrix(c(
+    0.9, eps, eps,
+    eps, 0.2, eps,
+    eps, eps, 0.8
+), byrow = TRUE, nrow = length(pir), ncol = length(pic))
 
-alpha <- switch(struct,
-    "modular" = {
-        alpha <- matrix(c(
-            0.9, eps, eps,
-            eps, 0.2, eps,
-            eps, eps, 0.8
-        ), byrow = TRUE, nrow = length(pir), ncol = length(pic))
-    },
-    "nested" = {
-        alpha <- matrix(c(
-            0.9, 0.65, 0.1,
-            0.35, 0.15, eps,
-            0.1, eps, eps
-        ), byrow = TRUE, nrow = length(pir), ncol = length(pic))
-    }
-)
+alpha_nested <- matrix(c(
+    0.9, 0.65, 0.1,
+    0.35, 0.15, eps,
+    0.1, eps, eps
+), byrow = TRUE, nrow = length(pir), ncol = length(pic))
 
 max_repetition <- 10L
 
 # Collections
 collections <- list(
-    "iid" = c(
+    "iid" = list("modular" = c(
         generate_bipartite_collection(nr1, nc1,
             pir, pic,
-            alpha,
+            alpha_modular,
             M = 1,
             model = "iid",
             return_memberships = TRUE
         ),
         generate_bipartite_collection(nr2, nc2,
             pir, pic,
-            alpha,
+            alpha_modular,
             M = M - 1,
             model = "iid",
             return_memberships = TRUE
         )
-    ),
-    "pi" = c(
+    ), "nested" = c(
         generate_bipartite_collection(nr1, nc1,
             pir, pic,
-            alpha, 1,
-            model = "pi",
+            alpha_nested,
+            M = 1,
+            model = "iid",
             return_memberships = TRUE
         ),
         generate_bipartite_collection(nr2, nc2,
             pir, pic,
-            alpha, M - 1,
-            model = "pi",
+            alpha_nested,
+            M = M - 1,
+            model = "iid",
             return_memberships = TRUE
         )
-    ),
-    "rho" = c(
-        generate_bipartite_collection(nr1, nc1,
-            pir, pic,
-            alpha, 1,
-            model = "rho",
-            return_memberships = TRUE
+    )),
+    "pi" = list(
+        "modular" = c(
+            generate_bipartite_collection(nr1, nc1,
+                pir, pic,
+                alpha_modular, 1,
+                model = "pi",
+                return_memberships = TRUE
+            ),
+            generate_bipartite_collection(nr2, nc2,
+                pir, pic,
+                alpha_modular, M - 1,
+                model = "pi",
+                return_memberships = TRUE
+            )
         ),
-        generate_bipartite_collection(nr2, nc2,
-            pir, pic,
-            alpha, M - 1,
-            model = "rho",
-            return_memberships = TRUE
+        "nested" = c(
+            generate_bipartite_collection(nr1, nc1,
+                pir, pic,
+                alpha_nested, 1,
+                model = "pi",
+                return_memberships = TRUE
+            ),
+            generate_bipartite_collection(nr2, nc2,
+                pir, pic,
+                alpha_nested, M - 1,
+                model = "pi",
+                return_memberships = TRUE
+            )
         )
     ),
-    "pirho" = c(
-        generate_bipartite_collection(nr1, nc1,
-            pir, pic,
-            alpha, 1,
-            model = "pirho",
-            return_memberships = TRUE
+    "rho" = list(
+        "modular" = c(
+            generate_bipartite_collection(nr1, nc1,
+                pir, pic,
+                alpha_modular, 1,
+                model = "rho",
+                return_memberships = TRUE
+            ),
+            generate_bipartite_collection(nr2, nc2,
+                pir, pic,
+                alpha_modular, M - 1,
+                model = "rho",
+                return_memberships = TRUE
+            )
         ),
-        generate_bipartite_collection(nr2, nc2,
-            pir, pic,
-            alpha, M - 1,
-            model = "pirho",
-            return_memberships = TRUE
+        "nested" = c(
+            generate_bipartite_collection(nr1, nc1,
+                pir, pic,
+                alpha_nested, 1,
+                model = "rho",
+                return_memberships = TRUE
+            ),
+            generate_bipartite_collection(nr2, nc2,
+                pir, pic,
+                alpha_nested, M - 1,
+                model = "rho",
+                return_memberships = TRUE
+            )
+        )
+    ),
+    "pirho" = list(
+        "modular" = c(
+            generate_bipartite_collection(nr1, nc1,
+                pir, pic,
+                alpha_modular, 1,
+                model = "pirho",
+                return_memberships = TRUE
+            ),
+            generate_bipartite_collection(nr2, nc2,
+                pir, pic,
+                alpha_modular, M - 1,
+                model = "pirho",
+                return_memberships = TRUE
+            )
+        ),
+        "nested" = c(
+            generate_bipartite_collection(nr1, nc1,
+                pir, pic,
+                alpha_nested, 1,
+                model = "pirho",
+                return_memberships = TRUE
+            ),
+            generate_bipartite_collection(nr2, nc2,
+                pir, pic,
+                alpha_nested, M - 1,
+                model = "pirho",
+                return_memberships = TRUE
+            )
         )
     )
 )
@@ -117,6 +164,7 @@ collections <- list(
 conditions <- expand.grid(
     prop_NAs = seq(from = 0, to = 0.9, by = 0.1),
     model = c("iid", "pi", "rho", "pirho"),
+    struct = c("modular", "nested"),
     repetition = seq.int(max_repetition)
 )
 
@@ -130,8 +178,7 @@ if (!dir.exists(main_dir)) {
 
 start_time <- format(Sys.time(), "%d-%m-%Y_%H-%M-%S")
 temp_dir <- file.path(main_dir, paste0(
-    "tmp", start_time, "_",
-    sampling, "_", struct
+    "tmp", start_time
 ))
 
 if (!dir.exists(temp_dir)) {
@@ -139,13 +186,11 @@ if (!dir.exists(temp_dir)) {
 }
 
 file_save <- file.path(main_dir, paste0(
-    "NA_robustness_", start_time, "_", sampling,
-    "_", struct, "_1-", nrow(conditions), ".Rds"
+    "NA_robustness_", start_time, "_1-", nrow(conditions), ".Rds"
 ))
 
 message(
-    "Starting NA robustness simulation with ", sampling,
-    " sampling and ", struct, " structure."
+    "Starting NA robustness simulation."
 )
 
 library("future")
@@ -160,7 +205,8 @@ result_list <- future.apply::future_lapply(
         # Looping over conditions
         prop_NAs <- conditions[current, ][["prop_NAs"]]
         model <- as.character(conditions[current, ][["model"]])
-        bipartite_collection <- collections[[model]]
+        struct <- as.character(conditions[current, ][["struct"]])
+        bipartite_collection <- collections[[model]][[struct]]
 
         # This is a list of the M incidence matrices
         bipartite_collection_incidence <- lapply(seq.int(M), function(m) {
@@ -187,7 +233,7 @@ result_list <- future.apply::future_lapply(
         start_time <- Sys.time()
         mybisbmpop <- estimate_colBiSBM(
             netlist = bipartite_collection_incidence, colsbm_model = model,
-            nb_run = 1,
+            nb_run = 3L,
             global_opts = list(
                 nb_cores = parallel::detectCores() - 1, verbosity = 0,
                 backend = "future"
@@ -196,8 +242,8 @@ result_list <- future.apply::future_lapply(
         stop_time <- Sys.time()
 
         baseline_LBM <- estimate_colBiSBM(
-            netlist = bipartite_collection_incidence[[1]], colsbm_model = "iid",
-            nb_run = 1,
+            netlist = list(bipartite_collection_incidence[[1]]), colsbm_model = "iid",
+            nb_run = 3L,
             global_opts = list(
                 nb_cores = parallel::detectCores() - 1, verbosity = 0,
                 backend = "future"
@@ -220,6 +266,7 @@ result_list <- future.apply::future_lapply(
         out_data_frame <- data.frame(
             prop_NAs = prop_NAs,
             model = model,
+            struct = struct,
             repetition = conditions[current, ][["repetition"]],
             auc_LBM = auc_LBM,
             auc_colBiSBM = auc_colBiSBM,
@@ -239,9 +286,7 @@ result_list <- future.apply::future_lapply(
                 Z[[1]][[2]],
                 mybisbmpop[["best_fit"]][["Z"]][[1]][[2]]
             ),
-            elapsed_secs = difftime(stop_time, start_time, units = "sec"),
-            sampling = sampling,
-            struct = struct
+            elapsed_secs = difftime(stop_time, start_time, units = "sec")
         )
 
         message("Finished step ", current, "/", nrow(conditions), "\n")
