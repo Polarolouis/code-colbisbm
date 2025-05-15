2c11badcb0e00a8afc7b895067ba15d990771aa1
Louis <louis.lacoste@agroparistech.fr>
2025-05-15 17:21:13 +0200

    More on network clustering

--- analyze_network_clustering.R
+++ analyze_network_clustering.R
@@ -16,35 +16,36 @@ options(tikzDocumentDeclaration = "\\documentclass[10pt]{standalone}")
 
 ## ----impoting-data, echo = FALSE----------------------------------------------------------------------------------------------------------------------------
 # data_list <- lapply(filenames, function(file) lapply(readRDS(file), function(model) model$list_clustering))
-df_netclust <- readRDS("simulations/clustering/9collection/descending.Rds")
-df_netclust$clust <- "descending"
-df_ascending <- readRDS("simulations/clustering/9collection/ascending.Rds")
-df_ascending$clust <- "ascending"
-df_iid_on_pirho <- readRDS("simulations/clustering/9collection/iid_on_pirho.Rds")
-df_iid_on_pirho$clust <- "descending"
+df_netclust <- readRDS("simulations/clustering/30collection/descending.Rds")
+# df_netclust$clust <- "descending"
+# df_ascending <- readRDS("simulations/clustering/9collection/ascending.Rds")
+# df_ascending$clust <- "ascending"
+# df_iid_on_pirho <- readRDS("simulations/clustering/9collection/iid_on_pirho.Rds")
+# df_iid_on_pirho$clust <- "descending"
 
-df_netclust <- rbind(df_netclust, df_ascending, df_iid_on_pirho)
+# df_netclust <- rbind(df_netclust, df_ascending, df_iid_on_pirho)
 
 df_netclust$model <- factor(df_netclust$model, levels = c(
     "iid", "pi",
-    "rho", "pirho",
-    "iid_on_pirho"
+    "rho", "pirho"
+    # ,"iid_on_pirho"
 ))
 
-df_netclust$clust <- factor(df_netclust$clust, levels = c(
-    "ascending", "descending"
-))
+# df_netclust$clust <- factor(df_netclust$clust, levels = c(
+#     "ascending", "descending"
+# ))
 
 df_netclust$model <- df_netclust$model |>
     case_match("iid" ~ "$iid$",
         "pi" ~ "$\\pi$",
         "rho" ~ "$\\rho$",
-        "pirho" ~ "$\\pi\\rho$",
-        "iid_on_pirho" ~ "$iid$ on $\\pi\\rho$",
+        "pirho" ~ "$\\pi\\rho$"
+        # ,"iid_on_pirho" ~ "$iid$ on $\\pi\\rho$"
+        ,
         .ptype = factor(levels = c(
             "$iid$", "$\\pi$",
-            "$\\rho$", "$\\pi\\rho$",
-            "$iid$ on $\\pi\\rho$"
+            "$\\rho$", "$\\pi\\rho$"
+            # ,"$iid$ on $\\pi\\rho$"
         ))
     )
 
@@ -52,49 +53,88 @@ df_netclust$model <- df_netclust$model |>
 #| dpi = 300,
 #| fig.asp = 0.5,
 #| fig.cap = "\\label{}ARI of the partition obtained by clustering in function of $\\eps$"
-ari_plot <- ggplot(df_netclust) +
+(ari_plot <- ggplot(df_netclust) +
     aes(x = as.factor(epsilon), y = ARI) +
-    scale_color_okabe_ito(order = c(1L, 8L)) +
+    scale_color_okabe_ito(
+        order = 2L:9L,
+        # c(1L, 8L)
+    ) +
     scale_fill_okabe_ito(order = 2L:9L, alpha = 0.5) +
     xlab("$\\epsilon_{\\alpha}$") +
     guides(
         fill = guide_legend(title = "Model"),
-        color = guide_legend(title = "Clustering")
+        color = guide_legend(title = "Model")
+        # color = guide_legend(title = "Clustering")
     ) +
     ylab("ARI of the clustering") +
-    geom_boxplot(aes(fill = model, color = clust), notch = TRUE) +
-    theme_minimal()
+    geom_boxplot(aes(
+        fill = model, color = model
+        # , color = clust
+    ), notch = TRUE) +
+    theme_minimal())
 
-bicl_plot <- ggplot(df_netclust) +
-    aes(x = as.factor(epsilon), y = BICL) +
-    scale_color_okabe_ito(order = c(1L, 8L)) +
+(nbcollection_plot <- ggplot(df_netclust) +
+    aes(x = as.factor(epsilon), y = nb_collections) +
+    scale_color_okabe_ito(
+        order = 2L:9L,
+        # c(1L, 8L)
+    ) +
     scale_fill_okabe_ito(order = 2L:9L, alpha = 0.5) +
+    lims(
+        y = c(0, 8)
+    ) +
     xlab("$\\epsilon_{\\alpha}$") +
     guides(
         fill = guide_legend(title = "Model"),
-        color = guide_legend(title = "Clustering")
+        color = guide_legend(title = "Model")
+        # color = guide_legend(title = "Clustering")
     ) +
-    ylab("BICL of the clustering") +
-    geom_boxplot(aes(fill = model, color = clust)) +
-    theme_minimal()
+    ylab("$\\#$ collections in partition") +
+    geom_boxplot(aes(
+        fill = model, color = model
+        # , color = clust
+    ), notch = TRUE) +
+    geom_hline(aes(yintercept = 3, linetype = "True number"), color = "red", show.legend = TRUE) +
+    scale_linetype_manual(name = "", values = c("dotted")) +
+    theme_minimal())
+
+# bicl_plot <- ggplot(df_netclust) +
+#     aes(x = as.factor(epsilon), y = BICL) +
+#     scale_color_okabe_ito(order = c(1L, 8L)) +
+#     scale_fill_okabe_ito(order = 2L:9L, alpha = 0.5) +
+#     xlab("$\\epsilon_{\\alpha}$") +
+#     guides(
+#         fill = guide_legend(title = "Model"),
+#         color = guide_legend(title = "Clustering")
+#     ) +
+#     ylab("BICL of the clustering") +
+#     geom_boxplot(aes(fill = model, color = clust)) +
+#     theme_minimal()
 
 output_tikz_folder <- here(
     "figures",
-    "simulations", "clustering", "9collection"
+    "simulations", "clustering", "30collection"
 )
 
 if (!dir.exists(output_tikz_folder)) {
     dir.create(output_tikz_folder, recursive = TRUE)
 }
 
-# tikz(
-#     file = file.path(output_tikz_folder, "ari-clustering.tex"), width = 5L,
-#     height = 2,
-#     standAlone = TRUE
-# )
-# print(ari_plot)
-# dev.off()
+tikz(
+    file = file.path(output_tikz_folder, "ari-clustering.tex"), width = 5L,
+    height = 2,
+    standAlone = TRUE
+)
+print(ari_plot)
+dev.off()
 
+tikz(
+    file = file.path(output_tikz_folder, "nbcollections-clustering.tex"), width = 5L,
+    height = 2,
+    standAlone = TRUE
+)
+print(nbcollection_plot)
+dev.off()
 png(
     file = file.path(output_tikz_folder, "ari-clustering-descending.png"),
     width = 960,
@@ -104,10 +144,10 @@ print(ari_plot)
 dev.off()
 ari_plot
 png(
-    file = file.path(output_tikz_folder, "bicl-clustering-descending.png"),
+    file = file.path(output_tikz_folder, "nbcollections-clustering-descending.png"),
     width = 960,
     height = 960
 )
-print(bicl_plot)
+print(nbcollection_plot)
 dev.off()
-bicl_plot
+nbcollection_plot
