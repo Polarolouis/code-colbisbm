3f59efa4a6b4e06352b23e74521478df5b3dae35
Louis <louis.lacoste@agroparistech.fr>
2025-08-11 15:07:45 +0200

    Adding Doré base data, with aggregated and non aggregated versions

---
+++ dore_make_matrices.R
@@ -0,0 +1,25 @@
+library(here)
+library(dplyr)
+
+dore_df <- read.table(here("data", "dore", "interaction-data.txt"), sep = "\t", header = TRUE)
+
+dore_df <- dore_df |> mutate(plantfull = paste(plantorder, plantfamily, plantgenus, plantspecies, sep = "_"), insectfull = paste(insectorder, insectfamily, insectgenus, insectspecies, sep = "_"))
+
+# Matrices at network level
+
+dore_networks_matrices <- setNames(lapply(seq(1, max(dore_df$id_network)), function(id) {
+    table(dore_df[dore_df$id_network == id, c("insectfull", "plantfull")])
+}), nm = unique(dore_df$web))
+
+saveRDS(dore_networks_matrices, here("data", "dore", "dore_networks_matrices.Rds"))
+
+# Matrices at aggregated network level
+aggreg_names <- sapply(unique(dore_df$id_network_aggreg), function(id) {
+    names <- unique(dore_df[dore_df$id_network_aggreg == id, "web"])
+    merged_names <- ifelse(length(names) > 1L, paste(names, collapse = "-"), names)
+})
+dore_aggregated_network_matrices <- setNames(lapply(unique(dore_df$id_network_aggreg), function(id) {
+    as.matrix(table(dore_df[dore_df$id_network_aggreg == id, c("insectfull", "plantfull")]))
+}), nm = aggreg_names)
+
+saveRDS(dore_aggregated_network_matrices, here("data", "dore", "dore_aggregated_networks_matrices.Rds"))
