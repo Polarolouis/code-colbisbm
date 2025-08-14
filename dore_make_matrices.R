82d13d6c55aa7811665607b804c1beb7155729ad
Louis <louis.lacoste@agroparistech.fr>
2025-08-14 17:40:20 +0200

    Fixing format from table to matrix

--- dore_make_matrices.R
+++ dore_make_matrices.R
@@ -7,7 +7,8 @@ dore_df <- dore_df |> mutate(plantfull = paste(plantorder, plantfamily, plantgen
 
 # Matrices at network level
 dore_networks_matrices <- setNames(lapply(unique(dore_df$id_network), function(id) {
-    table(dore_df[dore_df$id_network == id, c("insectfull", "plantfull")])
+    adj_table <- table(dore_df[dore_df$id_network == id, c("insectfull", "plantfull")])
+    return(matrix(adj_table, ncol = ncol(adj_table), dimnames = dimnames(adj_table)))
 }), nm = unique(dore_df$web))
 
 saveRDS(dore_networks_matrices, here("data", "dore", "dore_networks_matrices.Rds"))
@@ -17,8 +18,9 @@ aggreg_names <- sapply(unique(dore_df$id_network_aggreg), function(id) {
     names <- unique(dore_df[dore_df$id_network_aggreg == id, "web"])
     merged_names <- ifelse(length(names) > 1L, paste(names, collapse = "-"), names)
 })
-dore_aggregated_network_matrices <- setNames(lapply(unique(dore_df$id_network_aggreg), function(id) {
-    as.matrix(table(dore_df[dore_df$id_network_aggreg == id, c("insectfull", "plantfull")]))
+dore_aggregated_networks_matrices <- setNames(lapply(unique(dore_df$id_network_aggreg), function(id) {
+    adj_table <- table(dore_df[dore_df$id_network_aggreg == id, c("insectfull", "plantfull")])
+    return(matrix(adj_table, ncol = ncol(adj_table), dimnames = dimnames(adj_table)))
 }), nm = aggreg_names)
 
-saveRDS(dore_aggregated_network_matrices, here("data", "dore", "dore_aggregated_networks_matrices.Rds"))
+saveRDS(dore_aggregated_networks_matrices, here("data", "dore", "dore_aggregated_networks_matrices.Rds"))
