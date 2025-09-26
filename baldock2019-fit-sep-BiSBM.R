4b45149893a57b0e88be838273a0cbaec03747f9
Louis <louis.lacoste@agroparistech.fr>
2025-09-26 13:53:13 +0200

    Fixing incomplete merge

--- baldock2019-fit-sep-BiSBM.R
+++ baldock2019-fit-sep-BiSBM.R
@@ -2,14 +2,10 @@ library(sbm)
 library(here)
 library(ggplot2) # For plotting
 library(patchwork) # For arranging plots
-<<<<<<< HEAD
 library(dplyr)
 library(tikzDevice) # For saving plots as .tex files
 library(knitr) # For kable
 library(colSBM) # For the colSBM model
-=======
-library(tikzDevice) # For saving plots as .tex files
->>>>>>> Adding first data
 
 # Set up tikzDevice to use standalone document class
 options(tikzDocumentDeclaration = "\\documentclass[10pt]{standalone}")
@@ -19,7 +15,6 @@ incidence_matrices <- readRDS(here("data", "baldock2019-binary-matrices.Rds"))
 
 # Fit the model
 set.seed(123)
-<<<<<<< HEAD
 fit_list <- lapply(seq_along(incidence_matrices), function(idx) {
     # fit <- estimateBipartiteSBM(
     #     netMat = incidence_matrix,
@@ -48,15 +43,6 @@ fit_list <- lapply(seq_along(incidence_matrices), function(idx) {
     )
     refit$optimize()
     return(refit)
-=======
-fit_list <- lapply(incidence_matrices, function(incidence_matrix) {
-    fit <- estimateBipartiteSBM(
-        netMat = incidence_matrix,
-        model = "bernoulli", dimLabels = c(row = "pollinators", col = "plants"),
-    )
-    return(fit)
->>>>>>> Adding first data
-})
 
 short_names <- c("Bristol", "Edinburgh", "Leeds", "Reading")
 
@@ -64,7 +50,6 @@ if (!dir.exists(here("figures", "applications", "baldock"))) {
     dir.create(here("figures", "applications", "baldock"), recursive = TRUE)
 }
 
-<<<<<<< HEAD
 plot_lbm_with_con <- function(fit, m = 1, title = NULL, oRow = NULL, oCol = NULL) {
     if (is.null(oRow)) {
         oRow <- order(fit$alpha %*% matrixStats::rowMeans2(sapply(fit$pi, function(pi) pi[[2]])), decreasing = TRUE)
@@ -95,111 +80,16 @@ plot_lbm_with_con <- function(fit, m = 1, title = NULL, oRow = NULL, oCol = NULL
         coord_equal(expand = FALSE) +
         theme_bw(base_size = 20, base_rect_size = 1, base_line_size = 1) +
         ggtitle(title) +
-        theme(axis.ticks = element_blank(), text = element_text(size = 30))
+        theme(axis.ticks = element_blank(), text = element_text(size = 30, family = "serif"))
     return(g)
 }
 
-
-=======
->>>>>>> Adding first data
-# Save the plots
-lapply(seq_along(fit_list), function(i) {
-    pdf(
-        here("figures", "applications", "baldock", paste0(
-            "baldock2019-fit-sep-BiSBM-",
-            short_names[i],
-            ".pdf"
-        )),
-        family = "Times"
-    )
-<<<<<<< HEAD
-
-    print(plot_lbm_with_con(fit_list[[i]], title = short_names[i]))
-    dev.off()
+plot_list <- lapply(seq_along(fit_list), function(fit_id) {
+    plot_lbm_with_con(fit = fit_list[[fit_id]], title = short_names[fit_id])
 })
 
+full_plot <- wrap_plots(plot_list, ncol = 2)
 
-# Retrieving clusters sizes per network
-clustersizes <- sapply(fit_list, function(fit) {
-    return(fit$nbBlocks)
-})
-# Rename rows
-# row.names(clustersizes) <- c("$\\hat{Q_1^m}$ Pollinators", "$\\hat{Q_2^m}$ Plants")
-# Prepare a kable for latex inclusion
-# kable(clustersizes,
-#     caption = "Results for separate BiSBM on \\cite{baldockSystemsApproachReveals2019}",
-#     col.names = short_names, format = "latex",
-#     escape = FALSE
-# ) |>
-#     cat()
 
-# Graphon distance matrix
-dist_mat <- outer(fit_list, fit_list, Vectorize(function(fit1, fit2) {
-    param1 <- fit1$parameters
-    param2 <- fit2$parameters
-
-    colSBM:::dist_graphon_symmetrization(
-        pis = list(param1$pi[[1]], param2$pi[[1]]),
-        rhos = list(param1$rho[[1]], param2$rho[[1]]),
-        alphas = list(param1$alpha, param2$alpha)
-    )
-}))
-
-dist_mat_marg <- outer(fit_list, fit_list, Vectorize(function(fit1, fit2) {
-    param1 <- fit1$parameters
-    param2 <- fit2$parameters
-
-    colSBM:::dist_graphon_marginals(
-        pis = list(param1$pi[[1]], param2$pi[[1]]),
-        rhos = list(param1$rho[[1]], param2$rho[[1]]),
-        alphas = list(param1$alpha, param2$alpha)
-    )
-}))
-
-# For version
-dist_mat_perm <- matrix(NA, nrow = 4, ncol = 4)
-for (i in 1:4) {
-    for (j in 1:i) {
-        fit1 <- fit_list[[i]]
-        fit2 <- fit_list[[j]]
-        param1 <- fit1$parameters
-        param2 <- fit2$parameters
-        dist_mat_perm[i, j] <- colSBM:::dist_graphon_marginals(
-            pis = list(param1$pi[[1]], param2$pi[[1]]),
-            rhos = list(param1$rho[[1]], param2$rho[[1]]),
-            alphas = list(param1$alpha, param2$alpha)
-        )
-    }
-}
-
-dist_mat_marginals <- matrix(NA, nrow = 4, ncol = 4)
-for (i in 1:4) {
-    for (j in 1:i) {
-        fit1 <- fit_list[[i]]
-        fit2 <- fit_list[[j]]
-        param1 <- fit1$parameters
-        param2 <- fit2$parameters
-        dist_mat_marginals[i, j] <- colSBM:::graphon_distance_identif(
-            pis = list(param1$pi[[1]], param2$pi[[1]]),
-            rhos = list(param1$rho[[1]], param2$rho[[1]]),
-            alphas = list(param1$alpha, param2$alpha)
-        )
-    }
-}
-=======
-    print(plot(fit_list[[i]], type = "data") +
-        labs(x = "Plants", y = "Pollinators") +
-        ggtitle(short_names[i]) +
-        theme(
-            strip.text.y = element_blank(),
-            strip.text.x = element_blank(),
-            panel.border = element_rect(
-                colour = "black",
-                fill = NA,
-                linewidth = 1
-            ),
-            text = element_text(size = 30)
-        ))
-    dev.off()
-})
->>>>>>> Adding first data
+ggsave(here("figures", "applications", "baldock", paste0(
+    "baldock2019-fit-sep-BiSBM.pdf")), full_plot)
