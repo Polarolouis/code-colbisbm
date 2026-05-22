cc39358ec51f86f1945fbd3170f1c1a9a268b49b
Louis <louis.lacoste@agroparistech.fr>
2026-05-22 11:13:38 +0200

    Simpler parallelization structure

--- simulations_computation_time.R
+++ simulations_computation_time.R
@@ -120,7 +120,7 @@ if (model != "iid") {
 options("future.globals.maxSize" = Inf)
 nbCores <- parallelly::availableCores(omit = 1L)
 nb_run <- 3L
-plan(list(tweak("callr", workers = floor(nbCores / nb_run)), tweak("callr", workers = nb_run)))
+plan(tweak("multisession", workers = nbCores))
 
 results_list <- lapply(seq_len(nrow(conditions)), function(row_idx) {
     Q1 <- conditions[row_idx, "Q1"]
@@ -130,7 +130,7 @@ results_list <- lapply(seq_len(nrow(conditions)), function(row_idx) {
 
     collection <- datasets[[row_idx]]
     start_time <- Sys.time()
-    fit_res <- colSBM::estimate_colBiSBM(netlist = collection, colsbm_model = model, global_opts = list(verbosity = 3L), nb_run = nb_run)
+    fit_res <- colSBM::estimate_colBiSBM(netlist = collection, colsbm_model = model, global_opts = list(verbosity = 3L, backend = "no_mc"), nb_run = nb_run)
     time_taken <- difftime(Sys.time(), start_time, units = "secs")
 
     res_df <- data.frame(model = model, Q1 = Q1, Q2 = Q2, rep = rep, duration = time_taken, correct_Q1 = fit_res$best_fit$Q[1] == Q1, correct_Q2 = fit_res$best_fit$Q[2] == Q2)
