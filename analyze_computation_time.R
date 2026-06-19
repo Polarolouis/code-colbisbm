c75d2604f9d4abd2636e8abe459bab713e5e7750
Louis <louis.lacoste@agroparistech.fr>
2026-06-19 10:08:26 +0200

    Fixing order of levels

--- analyze_computation_time.R
+++ analyze_computation_time.R
@@ -3,15 +3,14 @@ library(tidyverse)
 library(latex2exp)
 save_path <- "simulations/computation_time"
 
-M <- 3
+M <- 10
 nr <- nc <- 100
 
 pattern <- paste0("M_", M, "_nr_", nr, "_nc_", nc)
 
 rds_files <- list.files(save_path, include.dirs = FALSE, pattern = pattern)
 
-computation_complex_time_df <- do.call("rbind", lapply(file.path(save_path, rds_files), readRDS))
-# %>% mutate(model = TeX(model))
+computation_complex_time_df <- do.call("rbind", lapply(file.path(save_path, rds_files), readRDS)) %>% mutate(model = factor(model, levels = c("iid", "pirho"), labels = c("iid", "pirho")))
 
 # computation_complex_time_df <- readRDS(file.path(save_path, rds_files[1]))
 library(ggplot2)
@@ -40,8 +39,8 @@ library(latex2exp)
 (q1_comp_plot <- ggplot(computation_complex_time_df %>% filter(Q2 == 8), aes(x = Q1, fill = model)) +
     geom_boxplot(aes(y = duration, group = interaction(Q1, model)), notch = TRUE) +
     geom_line(data = avg_computation %>% filter(Q2 == 8), aes(y = avg, color = model)) +
-    scale_fill_manual(values = model_colors[c(1, 4)], labels = c("iid", TeX("$\\pi\\rho$-colBiSBM"))) +
-    scale_color_manual(values = model_colors[c(1, 4)], labels = c("iid", TeX("$\\pi\\rho$-colBiSBM"))) +
+    scale_fill_manual(values = model_colors[c(1, 4)], drop = FALSE, labels = c("iid", TeX("$\\pi\\rho$-colBiSBM"))) +
+    scale_color_manual(values = model_colors[c(1, 4)], drop = FALSE, labels = c("iid", TeX("$\\pi\\rho$-colBiSBM"))) +
     scale_x_continuous(breaks = seq(2, 8)) +
     labs(fill = "Model", color = "Model", y = "Time (s)", x = TeX("$Q_1$")) +
     ggtitle(TeX(paste0("$n_1=n_2=", nr, ",M=", M, "$"))) +
@@ -50,8 +49,8 @@ library(latex2exp)
 (q2_comp_plot <- ggplot(computation_complex_time_df %>% filter(Q1 == 8), aes(x = Q2, fill = model)) +
     geom_boxplot(aes(y = duration, group = interaction(Q2, model)), notch = TRUE) +
     geom_line(data = avg_computation %>% filter(Q1 == 8), aes(y = avg, color = model)) +
-    scale_fill_manual(values = model_colors[c(1, 4)], labels = c("iid", TeX("$\\pi\\rho$-colBiSBM"))) +
-    scale_color_manual(values = model_colors[c(1, 4)], labels = c("iid", TeX("$\\pi\\rho$-colBiSBM"))) +
+    scale_fill_manual(values = model_colors[c(1, 4)], drop = FALSE, labels = c("iid", TeX("$\\pi\\rho$-colBiSBM"))) +
+    scale_color_manual(values = model_colors[c(1, 4)], drop = FALSE, labels = c("iid", TeX("$\\pi\\rho$-colBiSBM"))) +
     scale_x_continuous(breaks = seq(2, 8)) +
     labs(fill = "Model", color = "Model", y = "Time (s)", x = TeX("$Q_2$")) +
     theme_minimal())
